61e51d677292b08dc88c3b8feb740f3eef3fe7db
Puchoanimal <santiagovera280@gmail.com>
2023-03-06 21:20:15 -0300

    responsive paginado64

--- client/src/Components/General/TypesPokemonInfo.jsx
+++ client/src/Components/General/TypesPokemonInfo.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import estilos from "./TypesPokemonInfo.module.css";
+/* import estilos from "./TypesPokemonInfo.module.css"; */
 
 
 const TypesPokemonInfo = ({ type }) => { 
 	return (
 		<ul >
 			{type?.map((type) => ( 
-				<li className={estilos.label}  key={type.name} value={type.name}>
+				<li   key={type.name} value={type.name}>
 					{type.name} 
 				</li>
 			))}
