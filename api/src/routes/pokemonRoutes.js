e8fd8bb8b64bfafa751778ce1cb34217e1e98d50
Puchoanimal <santiagovera280@gmail.com>
2023-03-29 15:05:38 -0300

    * en app

--- api/src/routes/pokemonRoutes.js
+++ api/src/routes/pokemonRoutes.js
@@ -5,11 +5,11 @@ const axios = require("axios")
 const {Op} = require("sequelize")
 const cors = require("cors")
 
-
+router.use(cors());
 //OBTENER TODOS LOS POKEMON EN RUTA '/' O POR QUERY
 
 
-router.get("/",cors(), async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   let name = req.query.name;
   if (name) {
     name = name.toLowerCase(); 
@@ -154,7 +154,7 @@ router.get("/",cors(), async (req, res, next) => {
 //OBTENER POKEMONS POR PARAMS {ID}
 
 //OBTENER POKEMON DE API POR PARAMS {ID}
-router.get("/:id",cors(), async (req, res, next) => { 
+router.get("/:id", async (req, res, next) => { 
   let id = req.params.id; 
   if (id.length < 5) { 
     try {
@@ -221,9 +221,9 @@ router.get("/:id",cors(), async (req, res, next) => {
 
 //CREAR UN POKEMON en bd
 
-router.options("/", cors());
 
-router.post("/",cors(), async (req, res, next) => {
+
+router.post("/", async (req, res, next) => {
   try {
     const { name, hp, attack, defense,  speed,  height, weight, image, type, description, } = req.body; 
   
--- api/src/routes/typeRoutes.js
+++ api/src/routes/typeRoutes.js
@@ -1,4 +1,4 @@
-const { Router} = require("express");
+/* const { Router} = require("express");
 const router = Router()
 const {Type } = require("../db") 
 const axios = require("axios")
@@ -27,4 +27,33 @@ router.get("/", cors(),  async (req, res, next) => {
     
 
 
+module.exports = router; */
+
+const { Router} = require("express");
+const router = Router()
+const {Type } = require("../db") 
+const axios = require("axios")
+const {Op} = require("sequelize")
+const cors = require("cors")
+
+router.get("/", cors(), async (req, res, next) => {
+    try {
+      let allTypes = await axios.get("https://pokeapi.co/api/v2/type"); 
+      let todosTypes = allTypes.data.results; 
+
+      todosTypes.forEach((item) => { 
+        Type.findOrCreate({ 
+          where: { 
+            name: item.name,  
+            },
+        });
+        });
+      const typesDb = await Type.findAll(); 
+      res.send(typesDb);  
+    } catch (error) {
+      return res.status(404).send("Error, no encuentra el type: " + error);
+        //next(error);
+    }
+    });
+    
 module.exports = router;
